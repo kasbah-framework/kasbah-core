279a9ed70d83ec8d9ab4f81639a42f79040ec36f
Brendan McKenzie <hello@brendanmckenzie.com>
2015-10-25 11:31:47 +1100

    Added tests scaffolding

---
+++ test/Kasbah.Core.ContentTreeTests/Tests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Kasbah.Core.ContentTree.Tests
+{
+    public class ContentTreeTests
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
