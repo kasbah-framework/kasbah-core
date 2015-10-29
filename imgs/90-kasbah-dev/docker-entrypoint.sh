63dc3171d92340b5485570dbb5518f07b73d108f
Brendan McKenzie <hello@brendanmckenzie.com>
2015-10-29 15:35:53 +1100

    Fixing Dockerfiles

--- imgs/90-kasbah-dev/docker-entrypoint.sh
+++ imgs/90-kasbah-dev/docker-entrypoint.sh
@@ -1,5 +1,5 @@
-sh /redis-entrypoint.sh &  PID_REDIS=$!
 /opt/solr/bin/solr -f & PID_SOLR=$!
-sh /pgsql-entrypoint.sh & PID_PGSQL=$!
+/redis-entrypoint.sh redis-server & PID_REDIS=$!
+/docker-entrypoint.sh postgres & PID_PGSQL=$!
 
 wait $PID_REDIS && wait $PID_SOLR && wait $PID_PGSQL
