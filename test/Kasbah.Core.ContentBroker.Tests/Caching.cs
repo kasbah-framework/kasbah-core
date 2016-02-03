4904f9ec2685af162743b16cb94baa0ece7d3572
Brendan McKenzie <hello@brendanmckenzie.com>
2016-02-04 08:01:29 +1100

    Refactoring out magic strings. Adding tests for caching

---
+++ test/Kasbah.Core.ContentBroker.Tests/Caching.cs
@@ -0,0 +1,44 @@
+using System;
+using Kasbah.Core.Cache;
+using Kasbah.Core.ContentTree;
+using Kasbah.Core.Events;
+using Kasbah.Core.Index;
+using Kasbah.Core.Utils;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Kasbah.Core.ContentBroker.Tests
+{
+    public class Caching
+    {
+        #region Public Methods
+
+        [Fact]
+        public void CreateNode_InvalidatesCorrectCacheKeys()
+        {
+            // Arrange
+            var alias = Guid.NewGuid().ToString();
+            var type = TypeUtil.TypeName<ExampleItem>();
+
+            var provider = new Mock<IDistributedCache>();
+            provider.Setup(e => e.Remove(ContentBroker.CacheKeys.Children(null))).Verifiable();
+
+            var cacheService = new CacheService(provider.Object);
+
+            var service = MockContentBroker(cacheService);
+
+            // Act
+            service.CreateNode<ExampleItem>(null, alias);
+
+            // Assert
+            provider.Verify();
+        }
+
+        #endregion
+
+        static ContentBroker MockContentBroker(CacheService cacheService)
+            => new ContentBroker(new ContentTreeService(Mock.Of<IContentTreeProvider>()), new IndexService(Mock.Of<IIndexProvider>()), new EventService(Mock.Of<IEventBusProvider>()), cacheService, Mock.Of<ILoggerFactory>());
+    }
+}
