a45920bd136282ca43ee5cdc003c779a3130f0df
Brendan McKenzie <hello@brendanmckenzie.com>
2016-02-09 08:05:11 +1100

    Added stream extensions

---
+++ src/Kasbah.Core/Extensions/StreamExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace Kasbah.Core
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToArray(this Stream stream)
+        {
+            if (stream is MemoryStream)
+            {
+                return ((MemoryStream)stream).ToArray();
+            }
+            else
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public static string SHA1(this Stream stream)
+        {
+            using (var sha = System.Security.Cryptography.SHA1.Create())
+            {
+                var checksum = sha.ComputeHash(stream);
+                return  BitConverter.ToString(checksum).Replace("-", string.Empty);
+            }
+        }
+    }
+}
