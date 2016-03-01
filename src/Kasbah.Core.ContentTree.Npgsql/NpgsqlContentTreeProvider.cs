25a0d28ea296c15975ed2c03bf1b8aa05e0d39f8
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-03-02 10:36:16 +1100

    Removing useless lock(){}. Remove stale code

--- src/Kasbah.Core.ContentTree.Npgsql/NpgsqlContentTreeProvider.cs
+++ src/Kasbah.Core.ContentTree.Npgsql/NpgsqlContentTreeProvider.cs
@@ -11,11 +11,7 @@ namespace Kasbah.Core.ContentTree.Npgsql
     {
         #region Private Members
 
-#if ENABLE_SCHEMA_CHECK
-        static bool FirstRun = true;
-#endif
         readonly NpgsqlConnection _connection;
-        readonly object _lock = new object();
         readonly ILogger _log;
 
         #endregion
@@ -42,111 +38,61 @@ namespace Kasbah.Core.ContentTree.Npgsql
 
         /// <inheritdoc/>
         public void CreateNode(Guid id, Guid? parent, string alias, string type)
-        {
-            lock (_lock)
-            {
-                _connection.ExecuteFromResource("CreateNode", new { id, parent, alias, type });
-            }
-        }
+            => _connection.ExecuteFromResource("CreateNode", new { id, parent, alias, type });
 
         /// <inheritdoc/>
         public void Delete(Guid id)
-        {
-            lock (_lock)
-            {
-                _connection.ExecuteFromResource("Delete", new { id });
-            }
-        }
+            => _connection.ExecuteFromResource("Delete", new { id });
 
         /// <inheritdoc/>
         public void Dispose()
         {
+            _connection.Dispose();
         }
 
         /// <inheritdoc/>
         public IEnumerable<NodeVersion> GetAllNodeVersions(Guid id)
-        {
-            lock (_lock)
-            {
-                return _connection.QueryFromResource<NodeVersion>("GetAllNodeVersions", new { id });
-            }
-        }
+            => _connection.QueryFromResource<NodeVersion>("GetAllNodeVersions", new { id });
 
         /// <inheritdoc/>
         public Node GetChild(Guid? parent, string alias)
-        {
-            lock (_lock)
-            {
-                return _connection.QuerySingleFromResource<Node>("GetChild", new { parent, alias });
-            }
-        }
+            => _connection.QuerySingleFromResource<Node>("GetChild", new { parent, alias });
 
         /// <inheritdoc/>
         public IEnumerable<Node> GetChildren(Guid? id)
-        {
-            lock (_lock)
-            {
-                return _connection.QueryFromResource<Node>("GetChildren", new { id });
-            }
-        }
+            => _connection.QueryFromResource<Node>("GetChildren", new { id });
 
         /// <inheritdoc/>
         public Node GetNode(Guid id)
-        {
-            lock (_lock)
-            {
-                return _connection.QuerySingleFromResource<Node>("GetNode", new { id });
-            }
-        }
+            => _connection.QuerySingleFromResource<Node>("GetNode", new { id });
 
         /// <inheritdoc/>
         public IDictionary<string, object> GetNodeVersion(Guid id, Guid version)
         {
-            lock (_lock)
-            {
-                var data = GetRawNodeVersion(id, version);
+            var data = GetRawNodeVersion(id, version);
 
-                return data == null ? null : Deserialise(data.Data);
-            }
+            return data == null ? null : Deserialise(data.Data);
         }
 
         /// <inheritdoc/>
         public NodeVersion GetRawNodeVersion(Guid id, Guid version)
-        {
-            lock (_lock)
-            {
-                return _connection.QuerySingleFromResource<NodeVersion>("GetNodeVersion", new { id, version });
-            }
-        }
+            => _connection.QuerySingleFromResource<NodeVersion>("GetNodeVersion", new { id, version });
 
         /// <inheritdoc/>
         public void MoveNode(Guid id, Guid? parent)
-        {
-            lock (_lock)
-            {
-                _connection.ExecuteFromResource("MoveNode", new { id, parent });
-            }
-        }
+            => _connection.ExecuteFromResource("MoveNode", new { id, parent });
 
         /// <inheritdoc/>
         public NodeVersion Save(Guid id, Guid node, IDictionary<string, object> item)
         {
-            lock (_lock)
-            {
-                var data = Serialise(item);
+            var data = Serialise(item);
 
-                return _connection.QuerySingleFromResource<NodeVersion>("Save", new { id, node, data });
-            }
+            return _connection.QuerySingleFromResource<NodeVersion>("Save", new { id, node, data });
         }
 
         /// <inheritdoc/>
         public void SetActiveNodeVersion(Guid id, Guid? version)
-        {
-            lock (_lock)
-            {
-                _connection.ExecuteFromResource("SetActiveNodeVersion", new { id, version });
-            }
-        }
+            => _connection.ExecuteFromResource("SetActiveNodeVersion", new { id, version });
 
         #endregion
     }
